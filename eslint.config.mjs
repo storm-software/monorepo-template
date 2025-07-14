4c00dc1e5058bda4713c1a761e3a0fc043334ec9
Pat <pat@patsullivan.org>
2025-07-14 00:23:20 +0000

    chore(monorepo): Reformat script TypeScript files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@
  This code was released as part of the Monorepo Template project. Monorepo Template
  is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page at https://stormsoftware.com/license.
+ our licensing page at https://stormsoftware.com/licenses/projects/monorepo-template.
 
  Website:                  https://stormsoftware.com
  Repository:               https://github.com/storm-software/monorepo-template
--- tools/scripts/src/nuke.mjs
+++ tools/scripts/src/nuke.mjs
@@ -6,11 +6,11 @@
  This code was released as part of the Monorepo Template project. Monorepo Template
  is maintained by Storm Software under the Apache-2.0 license, and is
  free for commercial and private use. For more information, please visit
- our licensing page at https://stormsoftware.com/projects/monorepo-template/license.
+ our licensing page at https://stormsoftware.com/licenses/projects/monorepo-template.
 
  Website:                  https://stormsoftware.com
  Repository:               https://github.com/storm-software/monorepo-template
- Documentation:            https://docs.stormsoftware.com/projects/monorepo-template/
+ Documentation:            https://docs.stormsoftware.com/projects/monorepo-template
  Contact:                  https://stormsoftware.com/contact
 
  SPDX-License-Identifier:  Apache-2.0
