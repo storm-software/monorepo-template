a9c076835d8b1497c5cff021760916e8d2e4689a
Pat <pat@patsullivan.org>
2025-12-15 23:59:25 +0000

    chore(monorepo): Clean up the template to use latest repo standards

--- devenv.nix
+++ devenv.nix
@@ -3,14 +3,13 @@
   name = "storm-software/monorepo-template";
 
   dotenv.enable = true;
-  dotenv.filename = [".env" ".env.local"];
+  dotenv.filename = [
+    ".env"
+    ".env.local"
+  ];
   dotenv.disableHint = true;
 
   # https://devenv.sh/basics/
   env.DEFAULT_LOCALE = "en_US";
   env.DEFAULT_TIMEZONE = "America/New_York";
-
-  # https://devenv.sh/packages/
-  packages = [];
 }
-
